f78bb53dd850727001ec689e25eaf47c10951907
djonpereira@gmail.com <djonpereira@gmail.com>
2015-07-10 22:43:39 +0100

    another lost files

---
+++ WingtipToys/AddToCart.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using WingtipToys.Logic;
+
+namespace WingtipToys
+{
+    public partial class AddToCart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string rawId = Request.QueryString["ProductID"];
+            int productId;
+
+            if (!string.IsNullOrWhiteSpace(rawId) && int.TryParse(rawId, out productId))
+            {
+                using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+                {
+                    usersShoppingCart.AddToCart(Convert.ToInt16(rawId));
+                }
+            }
+            else {
+                Debug.Fail("ERROR : We should never get here...");
+                throw new Exception("ERROR: illegal operation");
+            }
+            Response.Redirect("ShoppingCart.aspx");
+                
+        }
+    }
+}
